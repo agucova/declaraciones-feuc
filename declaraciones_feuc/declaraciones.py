6148cca9d352116502c1fc9ea04b76a1009c1fb8
Agustín Covarrubias <agucova@uc.cl>
2020-06-10 17:32:37 -0400

    Añadir workflow OpenID-Google Cambié SQLAlchemy a peewee y arreglé el schema.

--- declaraciones_feuc/declaraciones.py
+++ declaraciones_feuc/declaraciones.py
@@ -1,35 +1,55 @@
 from flask import Flask
-from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import render_template, redirect, request, url_for
+
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+
+from oauthlib.oauth2 import WebApplicationClient
 import os
+import requests
+import json
 
-# create and configure the app
+from declaraciones_feuc.model import db, Person, Statement
+
 app = Flask(__name__, instance_relative_config=True)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///declaraciones.sqlite3"
-db = SQLAlchemy(app)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+# User session management setup
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-# Define the basic model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+# Auth setup
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
-    def __repr__(self):
-        return "<User %r>" % self.username
+# OAuth2 Client
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
 
 
-class Statement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    author = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_person(id):
+    return Person.get(id)
 
-    def __repr__(self):
-        return "<Post %r>" % self.title
+
+# DB connection per request
+@app.before_request
+def before_request():
+    db.connect()
+    db.create_tables([Person, Statement])
+
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
 
 
 # disable cache
@@ -43,24 +63,150 @@ def after_request(response):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return (
+        render_template("404.html", is_authenticated=current_user.is_authenticated),
+        404,
+    )
 
 
 @app.errorhandler(403)
 def page_forbidden(e):
-    return render_template("403.html"), 403
+    return (
+        render_template("403.html", is_authenticated=current_user.is_authenticated),
+        403,
+    )
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template("500.html"), 500
+    return (
+        render_template("500.html", is_authenticated=current_user.is_authenticated),
+        500,
+    )
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", is_authenticated=current_user.is_authenticated)
 
 
 @app.route("/declaraciones")
 def declaraciones():
-    return render_template("declaraciones.html")
+    return render_template(
+        "declaraciones.html", is_authenticated=current_user.is_authenticated
+    )
+
+
+@app.route("/admin")
+@login_required
+def admin():
+    return render_template("admin.html", is_authenticated=current_user.is_authenticated)
+
+
+# Here comes the auth
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return page_forbidden(403)
+
+
+@app.route("/login")
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        print(userinfo_response.json())
+        google_id = userinfo_response.json()["sub"]
+        email = userinfo_response.json()["email"]
+        first_name = (userinfo_response.json()["given_name"],)
+        last_name = userinfo_response.json()["family_name"]
+        name = userinfo_response.json()["name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Doesn't exist? Add to database
+    if not Person.select().where(Person.google_id == google_id).exists():
+        Person.create(
+            google_id=google_id,
+            username=email.split("@")[0],
+            email=email,
+            name=name,
+            first_name=first_name,
+            last_name=last_name,
+            isRepresentative=False,
+            is_active=True,
+            is_authenticated=True,
+        )
+
+    user = Person.get(Person.google_id == google_id)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("home"))
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+
+if __name__ == "__main__":
+    app.run(ssl_context=("cert.pem", "key.pem"))
---
+++ declaraciones_feuc/model.py
@@ -0,0 +1,72 @@
+from peewee import (
+    Model,
+    Check,
+    CharField,
+    BooleanField,
+    IntegerField,
+    BigIntegerField,
+    DateField,
+    ForeignKeyField,
+    SqliteDatabase,
+)
+
+
+db = SqliteDatabase("statements.sqlite")
+
+# Model
+class Person(Model):
+    google_id = CharField(null=False, max_length=400)
+    first_name = CharField(null=False)
+    last_name = CharField(null=False)
+    name = CharField(null=False)
+    username = CharField(null=False)
+    email = CharField(null=False)
+    isRepresentative = BooleanField(
+        null=False,
+        help_text="Booleano representando si es un representante en el consejo o no.",
+    )
+    typeOfRepresentative = CharField(
+        null=True,
+        max_length=120,
+        help_text="Tipo de representante. Ej. Consejero Territorial",
+    )
+    territory = CharField(
+        null=True,
+        max_length=120,
+        help_text="Territorio Político. Ej. Ciencias de la Salud",
+    )
+    carreer = CharField(
+        null=True, max_length=120, help_text="Carrera. Ej. Fonoaudiología"
+    )
+    year = IntegerField(
+        null=True,
+        constraints=[Check("3000 > year > 2018")],
+        help_text="Año del cargo tomado por el representante. Se aproxima según el año completo, si empieza en Diciembre 2019 es 2020.",
+    )
+    is_active = BooleanField(null=False)
+    is_authenticated = BooleanField(null=False)
+
+    class Meta:
+        database = db
+
+
+class Statement(Model):
+    title = CharField(
+        max_length=400,
+        null=False,
+        help_text='Título de la declaración. Por favor no incluir "Declaración sobre".',
+    )
+    date_added = DateField(
+        null=False, help_text="Fecha en la que fue añadida la declaración a la db."
+    )
+    submited_by = ForeignKeyField(
+        Person,
+        backref="statements",
+        help_text="Foreign key de quién agregó la declaración.",
+    )
+
+    class Meta:
+        database = db
+
+
+db.create_tables([Person, Statement])
