2432fb63e3c2e6b5096065ccf4ad6c4affd64341
Agustín Covarrubias <agucova@uc.cl>
2020-06-11 16:48:29 -0400

    Remove unused argument

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -3,7 +3,7 @@ import subprocess
 
 def when_ready(server):
     open("/tmp/app-initialized", "w").close()
-    subprocess.run(["$(npm bin)/companion", "-l"], shell=True)
+    subprocess.run(["$(npm bin)/companion"], shell=True)
 
 
 bind = "unix:///tmp/nginx.socket"
