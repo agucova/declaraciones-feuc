dcabb7600ac1e50935055349209d4b42121a478f
Agustín Covarrubias <agucova@uc.cl>
2020-06-11 16:48:11 -0400

    Add companion

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,5 +1,9 @@
+import subprocess
+
+
 def when_ready(server):
     open("/tmp/app-initialized", "w").close()
+    subprocess.run(["$(npm bin)/companion", "-l"], shell=True)
 
 
 bind = "unix:///tmp/nginx.socket"
