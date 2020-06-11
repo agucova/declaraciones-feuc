2782f6120ed538cc8945638bc25f2c9bb7830043
Agustín Covarrubias <agucova@uc.cl>
2020-06-11 17:01:57 -0400

    Add companion to nginx

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,8 +1,10 @@
 import subprocess
 
-
+# Creates the file that signals nginx to start handling requests
+# Starts Uppy's Companion
 def when_ready(server):
     open("/tmp/app-initialized", "w").close()
+    subprocess.run(["mkdir", "companion/"], shell=True)
     subprocess.run(["$(npm bin)/companion"], shell=True)
 
 
